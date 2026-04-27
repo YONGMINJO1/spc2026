d84aee97cb9af1e9fce5da0b41f5622cd336bae4
조용민 <iy0594@naver.com>
2026-04-27 09:59:42 +0900

    refactor: 일반 함수 -> 화살표 함수로 수정
    -ieftTriangle 삼각형

--- 0.mini-projects/javascript-hw/1.ieftTriangle.js
+++ 0.mini-projects/javascript-hw/1.ieftTriangle.js
@@ -5,3 +5,10 @@ function ieftTriangle(lines) {
 }
 
 ieftTriangle(5); // 결과를 보기 위해 함수 사용
+
+// 화살표 함수로 변경
+const ieftTriangleArrow = (lines) => {for (let i=1; i <= lines; i++) { 
+  console.log('*'.repeat(i))};
+}
+
+ieftTriangleArrow(5);
