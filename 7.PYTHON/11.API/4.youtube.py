55bd9bf50386841a81722af9e17eba645492cf72
조용민 <iy0594@naver.com>
2026-05-15 17:19:19 +0900

    [NEW] youtube API

---
+++ 7.PYTHON/11.API/4.youtube.py
@@ -0,0 +1,13 @@
+# pip install python-dotenv
+# .env 파일 안에서 Key 관리
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = 'YOUTUBE_API_KEY'
+
+search_query = '파이썬 튜토리얼'
+
+params
