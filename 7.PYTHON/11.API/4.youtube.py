7dd7fbd23cb6c0f82e6588a81fe3fd46cdbf4b6c
YONGMINJO1 <iy0594@naver.com>
2026-05-18 00:34:48 +0900

    fix: youtube API

--- 7.PYTHON/11.API/4.youtube.py
+++ 7.PYTHON/11.API/4.youtube.py
@@ -4,8 +4,31 @@ import os
 import requests
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()  # .env 파일을 읽어서 해당 key/value를 메모리 (환경변수에 올려둠)
 
-API_KEY = 'YOUTUBE_API_KEY'
+API_KEY = os.getenv("YOUTUBE_API_KEY")
 
-search_query = '파이썬 튜토리얼'
+url = "https://www.googleapis.com/youtube/v3/search"
+
+search_query = "파이썬 튜토리얼"
+
+params = {
+    "part": "snippet",
+    "q": search_query,
+    "type": "video",
+    "maxResult": 50,
+    "key": API_KEY,
+}
+
+response = requests.get(url, params)
+data = response.json()
+print(data)
+
+for item in data["items"]:
+    title = item["snippet"]["title"]
+    video_id = item["id"]["videoId"]
+    video_url = f"https://www.youtube.com/watch?v={video_id}"
+    description = item["snippet"]["description"]
+
+    print(f"제목: {title}, URL: {video_url}, 설명: {description}")
+    print("-" * 48)
