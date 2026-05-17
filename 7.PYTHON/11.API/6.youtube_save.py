c87cf8041e0d4b353365d305f0c3a66bceeb5cb7
YONGMINJO1 <iy0594@naver.com>
2026-05-18 02:29:39 +0900

    fix: youtube 및 aistudio api

--- 7.PYTHON/11.API/6.youtube_save.py
+++ 7.PYTHON/11.API/6.youtube_save.py
@@ -5,20 +5,20 @@ import requests
 from dotenv import load_dotenv
 import csv
 
-load_dotenv() # .env파일을 읽어서 해당 key/value 를 메모리(환경변수)에 올려둠
+load_dotenv()  # .env파일을 읽어서 해당 key/value 를 메모리(환경변수)에 올려둠
 
 API_KEY = os.getenv("YOUTUBE_API_KEY")
 
-url = 'https://www.googleapis.com/youtube/v3/search'
+url = "https://www.googleapis.com/youtube/v3/search"
 
 search_query = "파이썬 튜토리얼"
 
 params = {
-    'part': 'snippet',
-    'q': search_query,
-    'type': 'video',
-    'maxResults': 50,
-    'key': API_KEY
+    "part": "snippet",
+    "q": search_query,
+    "type": "video",
+    "maxResults": 50,
+    "key": API_KEY,
 }
 
 response = requests.get(url, params)
@@ -27,15 +27,18 @@ print(data)
 
 # csv를 저장
 
-with open("search_reault.csv", "w", newline="", encoding="utf-8") as file:
+with open("search_result.csv", "w", newline="", encoding="utf-8") as file:
 
     writer = csv.writer(file)
-    writer.writerow(["title","video_id","video_url","description"])
-for item in data['items']:
-    title = item['snippet']['title']
-    video_id = item['id']['videoId']
-    video_url = f"https://www.youtube.com/watch?v={video_id}"
-    description = item['snippet']['description']
 
-    print(f'제목: {title}, URL: {video_url}, 설명: {description}')
-    print('-'*40)
+    writer.writerow(["title", "video_id", "video_url", "description"])
+    for item in data["items"]:
+        title = item["snippet"]["title"]
+        video_id = item["id"]["videoId"]
+        video_url = f"https://www.youtube.com/watch?v={video_id}"
+        description = item["snippet"]["description"]
+
+        print(f"제목: {title}, URL: {video_url}, 설명: {description}")
+        print("-" * 40)
+
+        writer.writerow([title, video_id, video_url, description])
