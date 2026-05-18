566ee4f78f65604e65dfa43bdf88cf6766829d29
YONGMINJO1 <iy0594@naver.com>
2026-05-19 00:08:12 +0900

    fix: 장바구니 ( 세션, 카트 )

--- 7.PYTHON/11.API/9.gamini.py
+++ 7.PYTHON/11.API/9.gamini.py
@@ -8,7 +8,7 @@ client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents="나는 한국에 살고 있는, 20대 개발자야. 오늘 저녁에 뭐 먹으면 좋은지 알려줘"
+    contents="나는 한국에 살고 있는, 20대 개발자야. 오늘 저녁에 뭐 먹으면 좋은지 알려줘",
 )
 
 print(response.text)
--- 7.PYTHON/16.session_cart/app.py
+++ 7.PYTHON/16.session_cart/app.py
@@ -1,21 +1,54 @@
-from flask import Flask,render_template,session,redirect,url_for
-
+from flask import Flask, render_template, session, redirect, url_for
 
 app = Flask(__name__)
-app.secret_key = 'Hello1234' # 나만 아는 나의 세션 암호화 키
+app.secret_key = "hello1234"  # 나만 아는 나의 세션 암호화 키
 
 items = [
+    {"id": "item1", "name": "햄버거", "price": 3000},
+    {"id": "item2", "name": "핫도그", "price": 2000},
+    {"id": "item3", "name": "콜라", "price": 1500},
 ]
 
-@app.route('/')
-def index():
-    return render_template('product.html', items = items)
 
-@app.route('/get-session')
-def get_session():
-    if 'username' in session:
-        return f"세션에서 당신의 정보를 찾았습니다. {session['username']}"
-    return "세션 정보가 없습니다."
+@app.route("/")
+def index():
+    return render_template("product.html", items=items)  # 여기 상품 채워넣기
+
+
+@app.route("/add_to_cart/<item_id>")
+def add_to_cart(item_id):
+    print("장바구니에 담을 상품: ", item_id)
+    if "cart" not in session:
+        session["cart"] = {}
+
+    if item_id in session["cart"]:
+        session["cart"][item_id] += 1
+    else:
+        # 장바구니에 담을 상품이 실제로 존재하는가?
+        session["cart"][item_id] = 1
+
+    print(session["cart"])
+    session.modified = True  # 세션 데이터가 수정되었음을 flask에게 인지시킴
+
+    return redirect(url_for("index"))
+
+
+@app.route("/cart")
+def view_cart():
+    cart_items = {}
+    total_price = 0
+
+    for item_id, quantity in session.get("cart", {}).items():
+        item = next((i for i in items if i["id"] == item_id), None)
+        cart_items[item_id] = {
+            "name": item["name"],
+            "quantity": quantity,
+            "price": item["price"],
+        }
+        total_price += item["price"] * quantity
+
+    # 여기에 장바구니에 담기 상품 채워 넣기
+    return render_template("cart.html", cart_items=cart_items, total_price=total_price)
 
 if __name__ == "__main__":
     app.run(debug=True)
