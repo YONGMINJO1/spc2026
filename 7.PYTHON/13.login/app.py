211a1d184d4b06de7921048f3698344716d4982a
조용민 <iy0594@naver.com>
2026-05-18 17:07:22 +0900

    [NEW] 블루프린트/디자인 그리고 로그인

--- 7.PYTHON/13.login/app.py
+++ 7.PYTHON/13.login/app.py
@@ -1,1 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# 사용자 로컬 DB
+users = [
+    {'name':'홍길동', 'id':'hong', 'pw':"1234"},
+    {'name':'고길동', 'id':'gil', 'pw':"abcd"},
+    {'name':'홍길동', 'id':'dong', 'pw':"qwe123"},
+]
+
+@app.route('/', method=['GET','POST'])
+def home():
+    if request.method == 'POST':
+        id = request.form['id']
+        pw = request.form['pw']
+        print(f"입력값: {id},{pw}")
+
+
+        user = None
+        for u in users:
+            if u['id'] == id and u ['pw'] == pw:
+                user = u
+
+        if user:
+            error = None
+        else: 
+            error = "Invalid ID or PW"
+        return render_template('index.html', user=user, error=error)
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
