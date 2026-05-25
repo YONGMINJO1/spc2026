4f4e4879ac47750efc37d1d1ccc153d2570e2175
YONGMINJO1 <iy0594@naver.com>
2026-05-26 01:13:59 +0900

    fix: ollama 와 로컬 모델의 활용

--- 9.openAI/7.ollama/1.qwen.py
+++ 9.openAI/7.ollama/1.qwen.py
@@ -1,30 +1,33 @@
 # pip install requests
 # ollama pull qwen2.5:1.5
+# ollama pull exaone3.5:2.4b
 
 import requests
 
 MODEL_NAME = "qwen2.5:1.5b"
+# MODEL_NAME = "exaone3.5:2.4b"
+# MODEL_NAME = "exaone3.5:latest"
+
 
 def ask_qwen(question):
 
     reponse = requests.post(
         "http://localhost:11434/api/generate",
-        json={
-            "model": MODEL_NAME,
-            "prompt": question,
-            "stream":False
-        }
+        json={"model": MODEL_NAME, "prompt": question, "stream": False},
     )
 
     data = reponse.json()
-    final_reply = data ['response']
-    
-    #print(final_reply)
-    
+    return data["response"]
+
+
+# print(ask_qwen("당신을 소개해주세요."))
+# print(ask_qwen("인공지능이란 무엇인가요?"))
+
+
 while True:
     user_input = input("나: ")
     if user_input == "exit":
         print("종료합니다.")
         break
 
-    print("응답: " )
+    print("응답: ")
