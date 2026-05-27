cac8a3f04122b1c81ad64bbee3b3e4f5316dc961
조용민 <iy0594@naver.com>
2026-05-27 17:20:15 +0900

    [NEW] 랭체인 시작

---
+++ 9.openAI/10.langchin/1.inro.py
@@ -0,0 +1,17 @@
+# pip install langchain langchain-openai
+
+import os
+from dotenv import load_dotenv
+
+# from langchain.llms import OpenAI # 구버전
+from langchain_openai import OpenAI # 신버전 
+
+load_dotenv()
+openai_api_key = os.environ.get("OPENAI_API_KEY")
+
+llm = OpenAI(model="gpt-4o-mini")
+print(llm)
+
+prompt = "오늘 저녁은 무엇을 먹을까요?"
+result = llm.invoke(prompt)
+print(result)
