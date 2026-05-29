3a3818ed7b8cc277a03b677aed330e19d95ebbea
조용민 <iy0594@naver.com>
2026-05-29 12:03:51 +0900

    [NEW] LangChain의 메모리 기능

---
+++ 10.LangChain/6.memory/1.review.py
@@ -0,0 +1,43 @@
+from dotenv import load_dotenv
+
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import HumanMessage, AIMessage
+
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+prompt = ChatPromptTemplate.from_messages([
+    {"system","당신은 친철한 챗봇입니다."},  # 나의 페르소나
+    {"user", "{input}"}                     # 나의 질문
+    # {"ai", "챗봇 답변"},
+    # {"user", "{input}"} 
+])
+
+chain = prompt | llm | StrOutputParser()
+print(chain.invoke({"input":"안녕하세요, 나는 홍길동입니다,"}))
+print(chain.invoke({"input":"그래서 내가 누구라고?"}))
+print(chain.invoke({"input":"아니! 방금 내가 말했잖아!! 왜 넌 그것도 몰라?"}))
+
+prompt_with_history = ChatPromptTemplate.from_messages([
+    {"system","당신은 친절한 챗봇입니다."},
+    MessagesPlaceholder("history"),
+    {"user":"{input}"}
+])
+
+chain2 = prompt_with_history | llm | StrOutputParser()
+
+history_example = [
+    HumanMessage(content="안녕하세요, 홍길동입니다."),
+    AIMessage(content="네, 홍길동님 반갑습니다.")
+]
+
+answer = chain2.invoke({
+    "history": history_example,
+    "input": "제 이름이 뭐죠?",
+})
+
+print(answer)
---
+++ 10.LangChain/6.memory/3.infile.py
@@ -0,0 +1,38 @@
+from dotenv import load_dotenv
+
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.chat_message_histories import FileChatMessageHistory
+
+load_dotenv()
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system","당신은 친절한 챗봇입니다."),
+    MessagesPlaceholder("histroy"),
+    ("user", "{input}"),
+])
+
+chain = prompt | llm | StrOutputParser()
+
+history = FileChatMessageHistory("history.json")
+
+# 프로세스 종료 시 삭제
+def chat(message):
+    print(f"질문: {message}")
+    answer = chain.invoke({
+        "input":message,
+        "histroy":history.messages, # 우리의 저장소에 있는 메시지 그대로 다
+        #"histroy":history.messages[-10], # 최근 10개의 대화만 기억 
+    })
+    print(f"답변: {answer}")
+    history.add_user_message(message)
+    history.add_ai_message(answer)
+
+# chat("안녕하세요..")
+# chat("제 이름은 곽길동입니다.")
+# chat("저는 겨울에 바닷가에 가서 서핑하는 것을 좋아합니다.")
+chat("제 이름과 취미가 뭐라고 했죠?")
