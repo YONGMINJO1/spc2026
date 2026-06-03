f3a9ed712c71863c529e8c61e9fa1459f4a5a859
YONGMINJO1 <iy0594@naver.com>
2026-06-04 08:01:53 +0900

    fix: 누락파일 업로드

--- 10.LangChain/8.agent/3.custom/1.mytool.py
+++ 10.LangChain/8.agent/3.custom/1.mytool.py
@@ -12,12 +12,17 @@ def calculator(expression: str) -> str:
 
     try:
         # 예외처리를 잘 하지 않으면, LLM에 지멋대로 입력하는 값으로 우리코드가 죽을 수 있음
-        return str(eval(expression))
+        # return str(eval(expression))
+        return str(
+            eval(expression, {"__builtins__": {}}, {})
+        )  # 내부 빌트인 함수들의 호출을 금지한다.
     except Exception as e:
         return f"계산 오류: {e}"
 
 
 llm = ChatOpenAI(model="gpt-4o-mini")
-agent = create_agent(llm, [calculator()])
+agent = create_agent(llm, [calculator])
 
-result = agent.invoke
+result = agent.invoke({"messages": [("user", "10 나누기 2 곱하기 5는?")]})
+
+print("최종답변: ", result["messages"][-1].content)
